dcf2340bc78415ca4af9b435d4067b665f481e37
Simon Casey <simon.jk.casey@gmail.com>
2016-11-09 14:36:58 +1300

    add hsb setups

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'OTD' });
 });
+
+/* GET squad list */
+
+/* GET player page by ID (by clicking link in squad list)*/
+
+/* GET fixture list */
+
+/* GET fixture by ID (by clicking link in fixture list)*/
+
+/* GET PAGE // POST FORM
+RESULT ENTRY, forced path >> bat input >> bowl input >> field input >> resukt input //OPTION TO UPLOAD IMAGE OF SCORESHEET?
+
+
+
+
 
 module.exports = router;
