a1473a8e46d529cc19d429cb24e71c7ac11ee4d7
Simon Casey <simon.jk.casey@gmail.com>
2016-11-23 21:55:24 +1300

    rem date from fake fixture seed for heroku deploy

--- seeds/fakeFixtures.js
+++ seeds/fakeFixtures.js
@@ -6,13 +6,13 @@ exports.seed = function(knex, Promise) {
       return Promise.all([
         // Inserts seed entries
         knex('fixtures').insert(
-          {id: 1, oppositionId: 1, groundId: 1, date: '2016-11-5'}),
+          {id: 1, oppositionId: 1, groundId: 1}),
         knex('fixtures').insert(
-          {id: 2, oppositionId: 4, groundId: 2, date: '2016-11-12'}),
+          {id: 2, oppositionId: 4, groundId: 2}),
         knex('fixtures').insert(
-          {id: 3, oppositionId: 6, groundId: 3, date: '2016-11-19'}),
+          {id: 3, oppositionId: 6, groundId: 3}),
         knex('fixtures').insert(
-          {id: 4, oppositionId: 2, groundId: 1, date: '2016-11-26'})
+          {id: 4, oppositionId: 2, groundId: 1})
       ]);
     });
 };
